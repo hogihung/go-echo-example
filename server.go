22e5e2f996ee72283af958b81de1f56084929d87
John F. Hogarty <hogihung@gmail.com>
2019-01-08 09:07:41 -0500

    Format imports.

--- server.go
+++ server.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"net/http"
 )
 
 func main() {
@@ -20,6 +21,19 @@ func main() {
 		return c.JSON(http.StatusCreated, "Welcome mvc echo with mysql app using Golang")
 	})
 
+	e.POST("/server/save", func(c echo.Context) error {
+		response := &jsonResponse{
+			Status:  "OK",
+			NumRows: "69",
+		}
+		return c.JSON(http.StatusOK, response)
+	})
+
 	e.Logger.Fatal(e.Start(":8081"))
 
+}
+
+type jsonResponse struct {
+	Status  string `json:"status"`
+	NumRows string `json:"num_rows"`
 }
